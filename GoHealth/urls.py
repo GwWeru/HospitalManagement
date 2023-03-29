99eb95046724cc3f32760857ae1808794ca39e7a
weru7 <georgeweru143@gmail.com>
2023-03-29 09:09:22 +0300

    new

--- GoHealth/urls.py
+++ GoHealth/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('contact.html', views.contact, name="contact"),
+    path('service.html', views.service, name="service"),
+    path('price.html', views.price, name="price"),
+    path('about.html', views.about, name="about"),
 ]
